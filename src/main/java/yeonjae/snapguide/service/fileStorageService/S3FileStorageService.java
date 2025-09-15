623cbc5c388f6668d7dfc9d9739f1d8ac1f87e1f
Yeonjae Kim <duswokim1220@gmail.com>
2025-09-15 15:00:57 +0900

    feat : change localFileStroage to S3FileStorage, and need to completly devide photos in storage. still acquire photo in local storage

--- src/main/java/yeonjae/snapguide/service/fileStorageService/S3FileStorageService.java
+++ src/main/java/yeonjae/snapguide/service/fileStorageService/S3FileStorageService.java
@@ -1,6 +1,8 @@
 package yeonjae.snapguide.service.fileStorageService;
 
+import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Date;
 import java.util.UUID;
 
 @Service
@@ -98,6 +102,45 @@ public class S3FileStorageService implements FileStorageService{
 
     }
 
+    /**
+     * Presigned URL을 생성하는 메서드
+     * @param filename 'images/web/' 경로를 제외한 순수 파일 이름 (예: uuid.jpg)
+     * @return 생성된 Presigned URL 문자열
+     */
+    public String generatePresignedUrl(String filename) {
+        // S3에 저장된 파일의 전체 경로 (Key)를 지정합니다.
+        // '/media/files/' 요청은 일반적으로 웹용 이미지를 의미하므로 'images/web/' 경로를 사용합니다.
+        String objectKey = "images/web/" + filename;
+
+        // URL의 유효시간을 설정합니다. (예: 10분)
+        Date expiration = new Date();
+        long expTimeMillis = expiration.getTime();
+        expTimeMillis += 1000 * 60 * 10; // 10분
+        expiration.setTime(expTimeMillis);
+
+        try {
+            // S3 객체가 실제로 존재하는지 먼저 확인합니다.
+            if (!amazonS3.doesObjectExist(bucketName, objectKey)) {
+                log.warn("S3에 존재하지 않는 파일에 대한 URL 생성 시도: {}", objectKey);
+                return null; // 파일이 없으면 null 반환
+            }
+
+            // Presigned URL 생성 요청을 만듭니다.
+            GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                    new GeneratePresignedUrlRequest(bucketName, objectKey)
+                            .withMethod(HttpMethod.GET)
+                            .withExpiration(expiration);
+
+            // URL 생성
+            URL url = amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
+            return url.toString();
+
+        } catch (Exception e) {
+            log.error("Presigned URL 생성 중 오류 발생: {}", e.getMessage());
+            return null;
+        }
+    }
+
     @Override
     public Resource downloadFile(String filePath) throws IOException {
         return null;
@@ -131,16 +174,5 @@ public class S3FileStorageService implements FileStorageService{
         return metadata;
     }
 
-    // 업로드 결과를 담을 DTO
-    public static class UploadResult {
-        public final String originalUrl;
-        public final String webUrl;
-        public final String thumbnailUrl;
 
-        public UploadResult(String originalUrl, String webUrl, String thumbnailUrl) {
-            this.originalUrl = originalUrl;
-            this.webUrl = webUrl;
-            this.thumbnailUrl = thumbnailUrl;
-        }
-    }
 }
