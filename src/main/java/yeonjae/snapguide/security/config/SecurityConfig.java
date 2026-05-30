2caebb46af7bef1ce02076ed505c133b4e18923d
Yeonjae Kim <duswokim1220@gmail.com>
2026-05-30 17:53:58 +0900

    fix(build): @Primary import 누락

--- src/main/java/yeonjae/snapguide/security/config/SecurityConfig.java
+++ src/main/java/yeonjae/snapguide/security/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
