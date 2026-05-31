d9b7b1f890a72675ff21631a8a32bc3ea6a05222
Yeonjae Kim <duswokim1220@gmail.com>
2026-06-01 01:42:28 +0900

    refactor(redis): remove custom RedisConnectionFactory, delegate to Spring Boot auto-config
    커스텀 RedisConnectionFactory 빈이 Spring Boot 자동 설정을 덮어써서
    spring.data.redis.password 등 외부 환경변수가 무시되는 구조적 문제 해결.
    RedisTemplate 직렬화 설정만 유지하고 연결 설정은 자동 설정에 위임.

--- src/main/java/yeonjae/snapguide/infrastructure/cache/redis/config/RedisConfig.java
+++ src/main/java/yeonjae/snapguide/infrastructure/cache/redis/config/RedisConfig.java
@@ -1,63 +1,24 @@
 package yeonjae.snapguide.infrastructure.cache.redis.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories // Redis 저장소 기능 활성화
+@EnableRedisRepositories
 public class RedisConfig {
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    @Value("${spring.data.redis.password:}")
-    private String password;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(this.host);
-        config.setPort(this.port);
-        if (password != null && !password.isBlank()) {
-            config.setPassword(this.password);
-        }
-        return new LettuceConnectionFactory(config);
-    }
-
-    // RedisTemplate 설정
-    // RedisTemplate은 DB 서버에 Set, Get, Delete 등을 사용할 수 있음
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        // Redis와 통신할 때 사용할 템플릿 설정
-        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        this.setKeyValueSerializer(redisTemplate);
-        this.setHashKeyValueSerializer(redisTemplate);
-//        this.setDefaultSerializer(redisTemplate);
-        return redisTemplate;
-    }
-
-//    private void setDefaultSerializer(RedisTemplate<String, Object> redisTemplate) {
-//        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
-//    }
-
-    // hash key, hash value에 대한 직렬화 방법 설정
-    private void setHashKeyValueSerializer(RedisTemplate<String, Object> redisTemplate) {
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-    }
-    // key, value에 대한 직렬화 방법 설정
-    private void setKeyValueSerializer(RedisTemplate<String, Object> redisTemplate) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
     }
 }
